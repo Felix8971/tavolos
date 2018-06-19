698d397f6866bc583f1b841b801aec27b9680774
LOTSYS\fdebon <ext.felix.debon@lotsys.com>
2018-06-19 17:26:42 +0200

    Footer two

--- src/components/FooterTwo.js
+++ src/components/FooterTwo.js
@@ -9,13 +9,25 @@ class FooterTwo extends React.Component {
     return (
       <div className='footer-two'>
         <div className='item'>
-          sdqqsqs
+          <a href="#" onClick={this.burgerToggle}><img className='logo' src={'./images/logo.png'} alt="Tavalos logo"/></a>
+          <h3>Tavolos</h3>
+          <a href="#" onClick={this.burgerToggle}>About us</a>
+          <a href="#" onClick={this.burgerToggle}>Blog</a>
+          <a href="#" onClick={this.burgerToggle}>Press</a>
+          <a href="#" onClick={this.burgerToggle}>Privacy Policy</a>
+          <a href="#" onClick={this.burgerToggle}>Terms & Conditions</a>
         </div>
         <div className='item'>
-          qdsqsqs
+          <h3>More</h3>
+          <a href="#" onClick={this.burgerToggle}>Partners</a>
+          <a href="#" onClick={this.burgerToggle}>Help & Support</a>
+          <a href="#" onClick={this.burgerToggle}>Contact Us</a>
         </div>
         <div className='item'>
-          qsdqsds
+          <h3>Restaurants</h3>
+          <a href="#" onClick={this.burgerToggle}>Find out more</a>
+          <a href="#" onClick={this.burgerToggle}>Restaurants FAQs</a>
+          <button>Register now</button>
         </div>
       </div>
     )
